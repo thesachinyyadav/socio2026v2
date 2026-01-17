86ed1e1c3fc50ecfd5b44360bcc99b1adc779b0a
Sachin Yadav <sachin.yadav@bcah.christuniversity.in>
2026-01-18 02:35:45 +0530

    prune: remove all files except keep_temp

--- client/app/create/fest/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import CreateFestForm from "../../_components/CreateFestForm";
-
-export default function CreateFestPage() {
-  return <CreateFestForm />;
-}
