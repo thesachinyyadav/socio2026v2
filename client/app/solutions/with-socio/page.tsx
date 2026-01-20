761b117d1b5eb6bf8b68621454b07dbb7352d620
Surya Vamshi <surya.s@bcah.christuniversity.com>
2026-01-20 16:37:03 +0530

    Fix: Correct Footer import path in with-socio page

--- client/app/solutions/with-socio/page.tsx
+++ client/app/solutions/with-socio/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Footer from "../_components/Home/Footer";
+import Footer from "../../_components/Home/Footer";
 
 const WithSocioPage = () => {
   const services = [
